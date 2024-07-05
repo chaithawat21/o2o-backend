036ccfb976b7e9888c955ebe20910dd079205236
prawit2101254411 <162869085+prawit2101254411@users.noreply.github.com>
2024-07-05 16:31:22 +0700

    AouthRout Update

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -12,19 +12,20 @@ module.exports = tryCatch(async (req, res, next) => {
       throw customError("Unauthorized", 401);
     }
     const token = authorization.split(" ")[1];
+    // console.log(token)
     if (!token) {
       throw customError("Unauthorized", 401);
     }
 
-    // const { id, s_code, t_code } = jwt.verify(token, process.env.JWT_SECRET);
-    // const result = t_code
-    //   ? await prisma.teacher.findUnique({ where: { t_code: t_code } })
-    //   : await prisma.student.findUnique({ where: { s_code: s_code } });
-    delete result.password;
-    req.user = result
+    const {id,firstname} = jwt.verify(token,process.env.JWT_SECRET)
+    const rs = await prisma.user.findFirst({
+      where: {
+        firstname: firstname
+      }
+    })
+    delete rs.password;
+    req.user = rs
     next()
-
-
 });
 
 
--- routes/authRount.js
+++ routes/authRount.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const authController = require('../controllers/auth-controller')
+const authenticate = require('../middlewares/authenticate')
 
 const authRoute = express.Router()
 
 authRoute.post('/register',authController.register)
+authRoute.post('/login', authController.login)
+authRoute.get('/me',authenticate ,authController.getme)
 
 
 module.exports = authRoute
