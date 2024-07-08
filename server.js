acbb165ad17cff1a8f1949510ad5c8af2b3060c7
chai <chaithawat.contact@gmail.com>
2024-07-08 17:16:48 +0700

    contact feature create backend

--- server.js
+++ server.js
@@ -1,27 +1,34 @@
-require('dotenv').config()
-const express =require('express')
-const cors = require('cors')
-const jwt =require('jsonwebtoken')
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+const jwt = require("jsonwebtoken");
 const prisma = require("./models");
 
-const notFound = require('./middlewares/not-found')
-const errorMiddleware = require('./middlewares/error-middleware')
-const authenticate = require('./middlewares/authenticate');
+const bodyParser = require('body-parser');
+const notFound = require("./middlewares/not-found");
+const errorMiddleware = require("./middlewares/error-middleware");
+const authenticate = require("./middlewares/authenticate");
+const contactRoute = require("./routes/contactRoute");
 
-const authRoute = require('./routes/authRount');
+const authRoute = require("./routes/authRount");
 
-const app = express()
+const app = express();
 
-// app.use(cors())
+app.use(cors());
 
-app.use(express.json())
+app.use(express.json());
 
-app.use('/auth', authRoute)
+app.use("/auth", authRoute);
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/about",contactRoute);
 
 // not found
-app.use( notFound )
+app.use(notFound);
 // error
-app.use(errorMiddleware)
+app.use(errorMiddleware);
 
-const port = process.env.PORT
-app.listen(port, () => console.log('server on', port))
+const port = process.env.PORT;
+app.listen(port, () => console.log("server on", port));
