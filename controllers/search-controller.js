d62f097afdb742764c2e3f152eda9d0e1aab20c5
Aphiwat <aphiwat.jantharasrisao@gmail.com>
2024-07-11 21:30:18 +0700

    select loan user from search

--- controllers/search-controller.js
+++ controllers/search-controller.js
@@ -1,11 +1,50 @@
 const tryCatch = require("../utils/tryCatch");
-const prisma = require('../models')
+const prisma = require("../models");
+const { join } = require("@prisma/client/runtime/library");
 
+module.exports.getTypeSearch = tryCatch(async (req, res, next) => {
+  const getRegions = await prisma.regions.findMany();
+  const getProvinces = await prisma.provinces.findMany();
+  const getCategories = await prisma.categories.findMany();
+  res.json({
+    regions: getRegions,
+    provinces: getProvinces,
+    categories: getCategories,
+  });
+});
 
-module.exports.getSearch = tryCatch(async(req,res,next) => {
-    const getRegions = await prisma.regions.findMany()
-    const getProvinces = await prisma.provinces.findMany()
-    const getCategories = await prisma.categories.findMany()
-    res.json({regions : getRegions, provinces : getProvinces ,categories :getCategories})
-})
+module.exports.getLoanUserOnSearch = tryCatch(async (req, res, next) => {
+  // const { regions, province, categories } = req.body;
+  // console.log(province);
+  const {province} = req.params
+  console.log(province);
+  // if (categories) {
+  //   const getLoanUser = await prisma.loan.findMany({
+  //     where: { categorie_id: categories },
+  //     include: {
+  //       borrower: true,
+  //     },
+  //   });
+  //   res.json(getLoanUser)
+  // }
+  if (province) {
+    const getLoanUser = await prisma.loan.findMany({
+      where: { business_address: +province },
+      include: {
+        borrower: true,
+      },
+    });
+    res.json(getLoanUser)
+  }
+});
 
+module.exports.getLoanById = tryCatch(async (req, res, next) => {
+  const { id } = req.params;
+  const getLoan = await prisma.loan.findUnique({
+    where: { id: +id },
+    include: {
+      borrower: true,
+    },
+  });
+  console.log(getLoan);
+});
